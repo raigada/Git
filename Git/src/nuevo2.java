4c0b1613d20a26b26f589a75e06726f80a4b6ecc
araigada <araigada@106-27.info2>
2012-05-18 13:47:18 +0200

    1 cambio en la rama 1

--- Git/src/nuevo2.java
+++ Git/src/nuevo2.java
@@ -8,7 +8,11 @@ public class nuevo2 {
 		System.out.println("probando ");
 		
 		System.out.println("2a accion ");
-
+		
+		
+		System.out.println("Este es 1 cambio en la rama1 ");
+		
+		System.out.println("rama 1, cambio 2 ");
 	}
 
 }
