abb5771e225448979c62cd92a0f5dfd4f303bbc6
araigada <araigada@106-27.info2>
2012-05-21 13:22:25 +0200

    Proyecto inicial

---
+++ araigada/src/clase1.java
@@ -0,0 +1,12 @@
+
+public class clase1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Soy la clase 1");
+
+	}
+
+}
---
+++ araigada/src/clase2.java
@@ -0,0 +1,12 @@
+
+public class clase2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Soy la clase 2");
+
+	}
+
+}
