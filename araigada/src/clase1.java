436d95781889c1158ec5b3da42c67b0317133f31
araigada <araigada@106-27.info2>
2012-05-21 14:00:52 +0200

    creando metodo1 en ambas clases

--- araigada/src/clase1.java
+++ araigada/src/clase1.java
@@ -4,6 +4,12 @@ public class clase1 {
 	/**
 	 * @param args
 	 */
+	public static void metodo1_clase1() {
+		
+
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Soy la clase 1");
 
--- araigada/src/clase2.java
+++ araigada/src/clase2.java
@@ -4,6 +4,12 @@ public class clase2 {
 	/**
 	 * @param args
 	 */
+	
+	public static void metodo1_clase2() {
+		
+
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Soy la clase 2");
 
